ec5a44f60e8354737cfc5913ec58f11ad312bdb0
austin shields <ajshields196@gmail.com>
2021-02-23 20:37:46 -0500

    add view for all_products path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, if: -> { request.format.html? }
 
   def display_all
-    render json: {message: "hello"}
+    @all_products = []
+    Product.all.each do |product|
+      current_product = {}
+      current_product[:name] = product[:name]
+      current_product[:price] = product[:price]
+      current_product[:description] = product[:description]
+      current_product[:image_url] = product[:image_url]
+      @all_products << current_product
+    end
+    
+    render "all_products.json.jb"
   end
 end
