b5121d720827861cfb27406c57accdfd7e0d379f
austin shields <ajshields196@gmail.com>
2021-02-25 20:54:26 -0500

    remove non RESTful routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,28 +4,5 @@ class Api::ProductsController < ApplicationController
     render 'index.json.jb'
   end
 
-  def display_by_id
-    user_input_id = params[:id].to_i
-    product = Product.find_by(id: user_input_id)
-
-    @product_info = {}
-    @product_info[:name] = product[:name]
-    @product_info[:price] = product[:price]
-    @product_info[:description] = product[:description]
-    @product_info[:image_url] = product[:image_url]
-    render 'display.json.jb'
-  end
-
-  def display
-    user_input_attribute = params[:attribute]
-    user_input_value = params[:value]
-    product = Product.find_by(user_input_attribute => user_input_value)
-
-    @product_info = {}
-    @product_info[:name] = product[:name]
-    @product_info[:price] = product[:price]
-    @product_info[:description] = product[:description]
-    @product_info[:image_url] = product[:image_url]
-    render "display.json.jb"
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,5 @@ Rails.application.routes.draw do
   # end
   namespace :api do
     get "/products" => "products#index"
-    get "/search_by_id" => "products#display_by_id"
-    get "/search_by_id/:id" => "products#display_by_id"
-
-    get "search_by/:attribute/:value" => "products#display"
   end
 end
