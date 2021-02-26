5f18712a6609ee378b75e82d41b3a967b49a68f5
austin shields <ajshields196@gmail.com>
2021-02-25 21:22:45 -0500

    add RESTful create route and initial update route

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -8,4 +8,19 @@ class Api::ProductsController < ApplicationController
     @product = Product.find_by(id: params[:id])
     render 'show.json.jb'
   end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      description: params[:description], 
+      price: params[:price], 
+      image_url: params[:image_url]
+    )
+    @product.save
+    render 'create.json.jb'
+  end
+
+  def update
+    render 'update.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     get "/products" => "products#index"
     get "/products/:id" => "products#show"
+    post "/products" => "products#create"
+    patch "/products/:id" => "products#update"
   end
 end
