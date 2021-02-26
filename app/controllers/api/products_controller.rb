0f57ab660c52bbbb9c645e84f35c4d9f837c453a
austin shields <ajshields196@gmail.com>
2021-02-25 21:43:40 -0500

    add initial RESTful delete path

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -21,6 +21,16 @@ class Api::ProductsController < ApplicationController
   end
 
   def update
+    @product = Product.find_by(id: params[:id])
+    @product[:name] = params[:name] || @product[:name]
+    @product[:description] = params[:description] || @product[:description]
+    @product[:price] = params[:price] || @product[:price]
+    @product[:image_url] = params[:image_url] || @product[:image_url]
+    @product.save
     render 'update.json.jb'
+  end
+
+  def destroy
+    render 'destroy.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
     get "/products/:id" => "products#show"
     post "/products" => "products#create"
     patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
   end
 end
