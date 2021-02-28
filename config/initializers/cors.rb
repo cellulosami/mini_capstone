099f353edfea3d68a14196601365a66a3f027ec7
austin shields <ajshields196@gmail.com>
2021-02-28 13:00:55 -0500

    add CORS thing badly and very confusedly

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,16 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'localhost:8080', 'mini-capstone.netlify.app'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
+
+
