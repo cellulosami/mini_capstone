40ca1b6c778032695a76ec81eee737cb9e6bf954
austin shields <ajshields196@gmail.com>
2021-03-01 22:11:17 -0500

    add is_discounted?, tax, and total methods to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    if price < 10
+      true
+    else
+      false
+    end
+  end
+
+  def tax
+    (price*0.09).round(2)
+  end
+
+  def total
+    price + tax
+  end
 end
