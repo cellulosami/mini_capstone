e9d9c079b7ff0fbd6bf800280560b421f340f129
austin shields <ajshields196@gmail.com>
2021-03-02 21:04:28 -0500

    update products description to be text datatype

---
+++ db/migrate/20210303020303_change_products_description_to_text.rb
@@ -0,0 +1,5 @@
+class ChangeProductsDescriptionToText < ActiveRecord::Migration[6.1]
+  def change
+    change_column :products, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_010836) do
+ActiveRecord::Schema.define(version: 2021_03_03_020303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_010836) do
     t.string "name"
     t.integer "price"
     t.string "image_url"
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
