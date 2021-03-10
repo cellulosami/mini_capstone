cdafabd5e61fe12792c5947feb2f7227ceb6c44c
austin shields <ajshields196@gmail.com>
2021-03-09 21:53:53 -0500

    allow orders create method to associate with current user

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -5,15 +5,23 @@ class Api::OrdersController < ApplicationController
   end
 
   def create
-    @order = Order.new(
-      product_id: params[:product_id],
-      user_id: params[:user_id],
-      quantity: params[:quantity],
-      subtotal: params[:subtotal],
-      tax: params[:tax],
-      total: params[:total]
-    )
-    @order.save
-    render "show.json.jb"
+    if current_user
+      @order = Order.new(
+        product_id: params[:product_id],
+        user_id: current_user.id,
+        quantity: params[:quantity],
+        subtotal: params[:subtotal],
+        tax: params[:tax],
+        total: params[:total]
+      )
+      @order.save
+      render "show.json.jb"
+    else
+      render json: { message: "yo what the hell" }
+    end
   end
 end
+
+#i have manual product creation. now i need:
+# - automatic association of user_id
+# - automatic calculation of subtotal, tax, and total
