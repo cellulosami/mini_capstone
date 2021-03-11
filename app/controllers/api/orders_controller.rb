52ee31e5a08afa4e4086e73eebff52d03eb14249
austin shields <ajshields196@gmail.com>
2021-03-10 20:48:15 -0500

    add user authentication for all orders actions

--- app/controllers/api/orders_controller.rb
+++ app/controllers/api/orders_controller.rb
@@ -1,4 +1,6 @@
 class Api::OrdersController < ApplicationController
+  before_action :authenticate_user
+  
   def index
     if current_user
       @orders = Order.where("user_id = ?", current_user.id)
