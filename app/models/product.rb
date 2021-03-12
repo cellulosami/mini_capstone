d3c964a121b3e39c2e56087683957d268db3fef1
austin shields <ajshields196@gmail.com>
2021-03-11 20:35:44 -0500

    add has_many through associations for categories and products

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,6 +7,7 @@ class Product < ApplicationRecord
   has_many :images
   has_many :orders
   has_many :category_products
+  has_many :categories, through: :category_products
 
   def is_discounted?
     if price < 10
