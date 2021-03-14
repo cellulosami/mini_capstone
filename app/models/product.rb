734200c87ebc06b998be514454e2bf0c17e578d0
austin shields <ajshields196@gmail.com>
2021-03-14 12:32:11 -0400

    add associations between users, orders, products, and carted products

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,9 +5,11 @@ class Product < ApplicationRecord
 
   belongs_to :supplier
   has_many :images
-  has_many :orders
+  has_many :orders, through: :carted_products
   has_many :category_products
   has_many :categories, through: :category_products
+  has_many :carted_products
+  has_many :users, through: :carted_products
 
   def is_discounted?
     if price < 10
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   has_many :orders
+  has_many :carted_products
+  has_many :products, through: :carted_products
 end
