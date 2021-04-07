e3e0c840238274e0cdfa9815b901f47ed78c4117
austin shields <ajshields196@gmail.com>
2021-04-07 19:24:06 -0400

    remove authentication for products index

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,26 +2,22 @@ class Api::ProductsController < ApplicationController
   before_action :authenticate_admin, only: [:create, :update, :destroy]
 
   def index
-    if current_user
-      if params[:search]
-        @products = Product.where("name ILIKE '%#{params[:search]}%'")
-      else
-        @products = Product.all
-      end
-
-      params[:sort] = params[:sort] || "id"
-      params[:sort_order] = params[:sort_order] || "asc"
-      @products = @products.order("#{params[:sort]} #{params[:sort_order].upcase}")
-
-      if params[:discount]
-        params[:discount_price] = params[:discount_price] || 20
-        params[:discount_price] = params[:discount_price].to_i
-        @products = @products.where("price < #{params[:discount_price]}")
-      end
+    if params[:search]
+      @products = Product.where("name ILIKE '%#{params[:search]}%'")
     else
-      @products = []
+      @products = Product.all
     end
-    
+
+    params[:sort] = params[:sort] || "id"
+    params[:sort_order] = params[:sort_order] || "asc"
+    @products = @products.order("#{params[:sort]} #{params[:sort_order].upcase}")
+
+    if params[:discount]
+      params[:discount_price] = params[:discount_price] || 20
+      params[:discount_price] = params[:discount_price].to_i
+      @products = @products.where("price < #{params[:discount_price]}")
+    end
+  
     p current_user
     render 'index.json.jb'
   end
